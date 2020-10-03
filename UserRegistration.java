1fac7c9647e45de11ca65721e27849bada961182
praveen3073 <pop3073@gmail.com>
2020-10-03 12:35:46 +0530

    Added: UC3 Email Validation

--- UserRegistration.java
+++ UserRegistration.java
@@ -22,6 +22,7 @@ public class UserRegistration {
 	{
 		String firstName;
 		String lastName;
+		String email;
 		do
 		{
 			System.out.print("Enter first name: ");
@@ -32,6 +33,11 @@ public class UserRegistration {
 			System.out.print("Enter last name: ");
 			lastName = in.nextLine();
 		}while(isValidLastName(lastName)==false);
+		do
+		{
+			System.out.print("Enter Email: ");
+			email = in.nextLine();
+		}while(isValidEmail(email)==false);
 	}
 	
 	/**
@@ -49,11 +55,31 @@ public class UserRegistration {
 		return true;
 	}
 	
+	/**
+	 * @param lastName
+	 * @return
+	 * Validates lastName, first char capital & min three char
+	 */
 	private static boolean isValidLastName(String lastName)
 	{
 		if(Pattern.matches("[A-Z][a-z]{2,}", lastName)==false)
 		{
 			System.out.println("Invalid Last Name.");
+			return false;
+		}
+		return true;
+	}
+	
+	/**
+	 * @param email
+	 * @return
+	 * Validates email, pattern abc.xyz@bl.co.in with .xyz & .in optional
+	 */
+	private static boolean isValidEmail(String email)
+	{
+		if(Pattern.matches("[a-z]+(.[a-z]+)?@bl.co(.in)?", email)==false)
+		{
+			System.out.println("Invalid Email.");
 			return false;
 		}
 		return true;
